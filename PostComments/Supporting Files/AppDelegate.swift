f0020121eedcf5f2669074b6c2bd2ab1cee9c438
Marlon Rugama <MarlonRugama@ac.c4q.nyc>
2018-02-03 11:30:34 -0500

    Status bar transparent

--- PostComments/Supporting Files/AppDelegate.swift
+++ PostComments/Supporting Files/AppDelegate.swift
@@ -22,6 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = NavC
         window?.makeKeyAndVisible()
+        application.statusBarStyle = .lightContent
         return true
     }
 
